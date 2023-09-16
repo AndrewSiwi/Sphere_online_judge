2b6b1ada9c77a71e58c868fdd741fcd55456d300
AndrewSiwi <67230305+AndrewSiwi@users.noreply.github.com>
2023-09-16 14:40:02 +0200

    create problem
    fctrl

---
+++ fctrl/fctrl.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+
+#define iter(var, start, cond, end, diff) for(int var = (start); var cond (end); var += diff)
+#define iter_fix(var, start, end) iter(var, start, <, end, 1)
+#define iter_arr(var) iter_fix(var, 0, this->n)
+#define iter_reverse(var, start, end) iter(var, start, >=, end, -1)
+
+#define iter_cont(var, cont) for(auto& var: cont)
+#define iter_cont_it(var, cont, comm) for(auto var = cont.begin(); var != cont.end(); comm)
+#define iter_cont_revit(var, cont, comm) for(auto var = cont.rbegin(); var != cont.rend(); comm)
+
+const int N = 1e9;
+
+class Solution
+{
+    static int t;
+    public: static void init()
+    {
+        cin >> t;
+        int ct = 0;
+        while(ct < t /* cin >> */)
+        {
+            Solution* sl = new Solution(ct++);
+            sl->process();
+            sl->print();
+            delete sl;
+        }
+    }
+
+
+    private: int ct;
+
+    int n, ret = 0;
+
+    public: Solution(int ct)
+    {   
+        this->ct = ct;
+
+        cin >> this->n;
+    }
+
+    void process()
+    {
+        int dev = 5;
+        while(dev <= N)
+        {
+            this->ret += this->n / dev;
+            dev *= 5;
+        }
+    }
+
+    void print()
+    {
+        cout << this->ret << "\n";
+        //cout << "\n";
+    }
+};
+
+int Solution::t = 1;
+
+#undef int
+int main(int argc, char* argv[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution::init();
+
+    return 0;
+}
